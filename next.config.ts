3cea3fdfa846ee7208378b908d8a7a72c52653ef
tonoy3125 <shaifshajedt@gmail.com>
2025-01-24 19:34:42 +0600

    next config domain added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["placeimg.com", "example.com", "res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
