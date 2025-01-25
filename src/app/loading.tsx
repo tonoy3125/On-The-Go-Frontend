47dcfb0b71a8fcc4d0f3caa01eceda329611dbc6
tonoy3125 <shaifshajedt@gmail.com>
2025-01-26 01:48:56 +0600

    loading design added

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  // Or a custom loading skeleton component
+  return (
+    <div className="min-h-screen center w-full">
+      <div className="h-8 w-8 border-4 border-t-primaryMat spinner rounded-full"></div>
+    </div>
+  );
+}
