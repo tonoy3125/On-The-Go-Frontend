799589715db4927475fd347ffd3c6f31bd255901
tonoy3125 <shaifshajedt@gmail.com>
2025-01-28 13:12:44 +0600

    reset password onsubmit function and error added

--- src/components/ResetPassword/ResetPassword.tsx
+++ src/components/ResetPassword/ResetPassword.tsx
@@ -1,11 +1,66 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
+import { useResetPasswordMutation } from "@/redux/features/auth/authApi";
+import { setUser } from "@/redux/features/auth/authSlice";
+import { useAppDispatch } from "@/redux/hook";
+import { verifyToken } from "@/utils/verifyToken";
 import Image from "next/image";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { toast } from "sonner";
 
 const ResetPassword = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm();
   const [showNewPassword, setShowNewPassword] = useState(false);
   const [showConfirmNewPassword, setShowConfirmNewPassword] = useState(false);
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+  const [resetPassword] = useResetPasswordMutation();
+  const searchParams = useSearchParams(); // Parse the query parameters
+  const email = searchParams.get("email"); // Get email from URL
+  const token = searchParams.get("token"); // Get token from URL
+  //   console.log(email);
+  //   console.log(token);
+
+  const newPassword = watch("newPassword");
+
+  const onSubmit = async (data: FieldValues) => {
+    const toastId = toast.loading("Proceeding to Reset Password");
+    try {
+      const newUpdatedPassword = {
+        token,
+        newPassword: data?.newPassword,
+        confirmNewPassword: data?.confirmNewPassword,
+      };
+      const res = await resetPassword(newUpdatedPassword).unwrap();
+      console.log(res);
+      const user = verifyToken(res?.data?.accessToken);
+      //   console.log(user);
+      dispatch(
+        setUser({
+          user: { user, id: res?.data?.user?._id },
+          token: res?.data?.accessToken,
+        })
+      );
+      toast.success(res.message || "Password reset to the successfully!", {
+        id: toastId,
+        duration: 3000,
+      });
+      router.push("/");
+    } catch (error: any) {
+      toast.error(error?.data?.message || "Something went wrong!", {
+        id: toastId,
+        duration: 3000,
+      });
+    }
+  };
 
   return (
     <div className="flex items-center justify-center lg:justify-start w-full md:gap-10 bg-[#FFFFFF]">
@@ -25,9 +80,9 @@ const ResetPassword = () => {
           Reset Password
         </h1>
         <p className="text-[#231928] font-medium mb-10 text-center lg:text-start">
-          Enter the email address associated with your account.
+          Enter a new password for {email}
         </p>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-5 relative">
             <h2 className="text-[15px] font-medium text-[#231928] mb-3 opacity-90">
               New Password
@@ -42,6 +97,13 @@ const ResetPassword = () => {
                   "border-color .15s ease-in-out, box-shadow .15s ease-in-out",
                 boxShadow: "0 0 0 0px bg-[#38b2ac]", // Default shadow
               }}
+              {...register("newPassword", {
+                required: "Password Can't be Blank.",
+                minLength: {
+                  value: 6,
+                  message: "Password is too short (minimum is 6 characters)",
+                },
+              })}
             />
             <span
               className="absolute right-4 md:right-3 top-[44px] rtl:left-0 rtl:right-auto "
@@ -55,6 +117,11 @@ const ResetPassword = () => {
                 <AiOutlineEye className="text-xl"></AiOutlineEye>
               )}
             </span>
+            {errors.newPassword && (
+              <p className="text-red-500 text-sm font-poppins font-medium pt-2">
+                {String(errors.newPassword.message)}
+              </p>
+            )}
           </div>
           <div className="mb-5 relative">
             <h2 className="text-[15px] font-medium text-[#231928] mb-3 opacity-90">
@@ -70,6 +137,12 @@ const ResetPassword = () => {
                   "border-color .15s ease-in-out, box-shadow .15s ease-in-out",
                 boxShadow: "0 0 0 0px bg-[#38b2ac]", // Default shadow
               }}
+              {...register("confirmNewPassword", {
+                required: "Password confirmation can't be blank",
+                validate: (value) =>
+                  value === newPassword ||
+                  "The password confirmation must match the provided password",
+              })}
             />
             <span
               className="absolute right-4 md:right-3 top-[44px] rtl:left-0 rtl:right-auto "
@@ -83,6 +156,11 @@ const ResetPassword = () => {
                 <AiOutlineEye className="text-xl"></AiOutlineEye>
               )}
             </span>
+            {errors.confirmNewPassword && (
+              <p className="text-red-500 text-sm font-poppins font-medium pt-2">
+                {String(errors.confirmNewPassword.message)}
+              </p>
+            )}
           </div>
           <input
             className="w-full py-2 bg-[#056464]  text-base font-poppins text-[#fff] font-medium rounded-lg border border-[#056464] mt-5 cursor-pointer"
