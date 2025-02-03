2006b228230989466a9c4fd66e5275b3f1de2d8b
tonoy3125 <shaifshajedt@gmail.com>
2025-02-03 12:49:12 +0600

    join group function added

--- src/components/GroupPage/GroupJoin/GroupJoin.tsx
+++ src/components/GroupPage/GroupJoin/GroupJoin.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Button } from "@/components/ui/button";
+import { useCurrentToken } from "@/redux/features/auth/authSlice";
 import { useJoinGroupMutation } from "@/redux/features/groupMember/groupMemberApi";
+import { useAppSelector } from "@/redux/hook";
 import { UsersRound } from "lucide-react";
 import { useState } from "react";
 import { FaSpinner } from "react-icons/fa";
@@ -14,12 +17,32 @@ interface IProps {
 const GroupJoin: React.FC<IProps> = ({ groupId, children, className }) => {
   const [joinGroup, { isLoading }] = useJoinGroupMutation();
   const [isJoined, setIsJoined] = useState(false);
-  
+  const token = useAppSelector(useCurrentToken);
+
+  const handleJoinGroup = async () => {
+    try {
+      const res = await joinGroup({ token, groupId });
+      console.log(res);
+      const error = res.error as any;
+      if (error) {
+        toast.error(error.data?.message || "Something went wrong");
+        return;
+      }
+
+      setIsJoined(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (isJoined) {
+    return <></>;
+  }
 
   return (
     children || (
       <Button
-        // onClick={handleJoinGroup}
+        onClick={handleJoinGroup}
         disabled={isLoading}
         variant="outline"
         className={`bg-primaryMat/10 text-primaryMat border-primaryMat center gap-[8px] ${
