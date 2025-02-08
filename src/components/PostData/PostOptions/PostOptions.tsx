9442310fea25d75ca973572ceb2cf33feec9b559
tonoy3125 <shaifshajedt@gmail.com>
2025-02-09 01:13:30 +0600

    post option added

--- src/components/PostData/PostOptions/PostOptions.tsx
+++ src/components/PostData/PostOptions/PostOptions.tsx
@@ -1,8 +1,80 @@
-const PostOptions = () => {
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { selectCurrentUser } from "@/redux/features/auth/authSlice";
+import { useAppSelector } from "@/redux/hook";
+import { IPost } from "@/types/post.types";
+import { TUserPayload } from "@/types/user.type";
+import { EllipsisVertical, Eye, Share2, Trash } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+import { toast } from "sonner";
+import { useCopyToClipboard } from "usehooks-ts";
+
+const PostOptions = ({ post }: { post: IPost }) => {
+  const user = useAppSelector(selectCurrentUser) as TUserPayload | null;
+  const isAuthor = user && post.user?._id === user?.user?._id;
+  const [_, copy] = useCopyToClipboard();
+
+ 
+
+  const handleShare = async () => {
+    const url = window.location.origin;
+    await copy(url + "/post/" + post._id);
+    toast.success("Shareable Link copied to clipboard");
+  };
+
   return (
-    <div>
-      <h1>This Is Post Option</h1>
-    </div>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" size={"icon"}>
+            <EllipsisVertical width={15} />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>Options</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem
+              onClick={handleShare}
+              className="flex items-center gap-[5px]"
+            >
+              <Share2 width={15} />
+              Share
+            </DropdownMenuItem>
+
+            <Link href={`/post/${post._id}`}>
+              <DropdownMenuItem className="flex items-center gap-[5px]">
+                <Eye width={15} /> View Post
+              </DropdownMenuItem>
+            </Link>
+            
+            {isAuthor ? (
+              <>
+                <DropdownMenuItem
+                  className="flex items-center gap-[5px]"
+                  onClick={() => setOpenDeleteModal(true)}
+                >
+                  <Trash width={15} />
+                  Delete
+                </DropdownMenuItem>
+              </>
+            ) : (
+              ""
+            )}
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      
+    </>
   );
 };
 
