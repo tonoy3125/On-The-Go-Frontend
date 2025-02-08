c64b15cfe2e5a67eb3ce0d3c011a150909d20c93
tonoy3125 <shaifshajedt@gmail.com>
2025-02-08 13:07:02 +0600

    get User Profile post api added

--- src/redux/features/post/postApi.ts
+++ src/redux/features/post/postApi.ts
@@ -1,4 +1,6 @@
+import { TResponseRedux } from "@/types/user.type";
 import { baseApi } from "../../api/baseApi";
+import { IPost } from "@/types/post.types";
 
 const PostApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
@@ -13,7 +15,44 @@ const PostApi = baseApi.injectEndpoints({
       }),
       invalidatesTags: ["Post"],
     }),
+    getUserProfilePost: builder.query({
+      query: (args) => {
+        const { token, ...params } = args; // Destructure token from args
+
+        const queryParams = new URLSearchParams();
+
+        // Loop through params and append them to queryParams
+        Object.keys(params).forEach((key) => {
+          if (Array.isArray(params[key])) {
+            // Handle array for categories (or other multiple values)
+            params[key].forEach((value: string) => {
+              queryParams.append(key, value);
+            });
+          } else if (params[key]) {
+            // Append normal key-value pairs
+            queryParams.append(key, params[key]);
+          }
+        });
+
+        return {
+          url: "/post/userPost",
+          method: "GET",
+          params: queryParams,
+          headers: {
+            Authorization: `Bearer ${token}`, // Include Authorization header
+          },
+        };
+      },
+      transformResponse: (response: TResponseRedux<IPost[]>) => {
+        console.log("inside redux", response);
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+      providesTags: ["Post"],
+    }),
   }),
 });
 
-export const { useCreatePostMutation } = PostApi;
+export const { useCreatePostMutation, useGetUserProfilePostQuery } = PostApi;
